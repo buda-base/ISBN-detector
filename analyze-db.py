5354561f4aa5a4f41949c7b177cd8cf57cbfe5d1
eroux <elie.roux@telecom-bretagne.eu>
2023-12-12 18:22:36 +0100

    first full version

--- analyze-db.py
+++ analyze-db.py
@@ -19,19 +19,44 @@ def normalize_isbn(isbn):
 
 def well_formed(isbn):
     # assumes isbn is normalized
-    match = re.search(r'^(\d{9}[0-9X]|\d{12}[0-9X])$', isbn)
+    match = re.search(r'^(\d{9}[0-9X]|\d{13})$', isbn)
     return True if match else False
 
 def valid(isbn):
     # assumes isbn is normalized
-    match = re.search(r'^(\d{9})(\d|X)$', isbn)
+    match = re.search(r'^(\d{9}|\d{12})(\d|X)$', isbn)
     if not match:
         return False
 
     digits = match.group(1)
     check_digit = 10 if match.group(2) == 'X' else int(match.group(2))
     result = sum((i + 1) * int(digit) for i, digit in enumerate(digits))
-    return (result % 11) == check_digit
+    if len(isbn) == 10:
+        return (result % 11) == check_digit
+    else:
+        return (result % 10) == check_digit
+
+def equivalent(isbn1, isbn2):
+    if isbn1 is None and isbn2 is None:
+        return True
+    if isbn1 is None or isbn2 is None:
+        return False
+    if len(isbn1) == len(isbn2):
+        return isbn1 == isbn2
+    if len(isbn1) not in [10, 13] or len(isbn2) not in [10, 13]:
+        return isbn1 == isbn2
+    if len(isbn1) == 10:
+        return isbn1[:9] == isbn2[3:12]
+    else:
+        return isbn1[3:12] == isbn2[:9]
+
+def has_equivalent_in(isbn, isbn_list):
+    if not isbn_list:
+        return False
+    for isbn_i in isbn_list:
+        if equivalent(isbn_i, isbn):
+            return True
+    return False
 
 def normalize_from_db(isbn):
     if '(' in isbn:
@@ -82,8 +107,8 @@ def get_w_to_mw():
 #     from_scans: [isbns]
 
 def analyze_w(w, w_dbinfo, mw, data, stats):
-    seen_isbns = []
     for ig, iginfo in w_dbinfo.items():
+        seen_isbns = []
         volnum = -1
         for imgfname, detections in iginfo.items():
             if imgfname == "n":
@@ -92,33 +117,35 @@ def analyze_w(w, w_dbinfo, mw, data, stats):
                     data["mw_info"][mw] = {"from_db": [], "from_scans": [], "per_ig": {}, "ig_to_vnum": {}}
                 data["mw_info"][mw]["ig_to_vnum"][ig] = volnum
                 continue
+            isbn = None
             for det in detections:
-                if det["t"] == "EAN13":
-                    if not det["d"].startswith("978") and not det["d"].startswith("979"):
-                        # we just ignore EANs that are not ISBNs
-                        continue
+                if det["t"] != "EAN13":
+                    continue
+                if (det["d"].startswith("978") or det["d"].startswith("979")) and well_formed(det["d"]):
                     isbn = det["d"]
-                    if isbn not in seen_isbns:
-                        if seen_isbns:
-                            print("two different isbns in "+w)
-                        seen_isbns.append(isbn)
-                        if isbn not in data["isbn_info"]:
-                            data["isbn_info"][isbn] = {}
-                        data["isbn_info"][isbn] = {}
-                        if mw not in data["isbn_info"][isbn]:
-                            data["isbn_info"][isbn][mw] = {}
-                        if w not in data["isbn_info"][isbn][mw]:
-                            data["isbn_info"][isbn][mw][w] = {}
-                        if ig not in data["isbn_info"][isbn][mw][w]:
-                            data["isbn_info"][isbn][mw][w][ig] = []
-                        data["isbn_info"][isbn][mw][w][ig].append(imgfname)
-                    if mw not in data["mw_info"]:
-                        data["mw_info"][mw] = {"from_db": [], "from_scans": [], "per_ig": {}, "ig_to_vnum": {}}
-                    data["mw_info"][mw]["from_scans"].append(isbn)
-                    if ig not in data["mw_info"][mw]["per_ig"]:
-                        data["mw_info"][mw]["per_ig"][ig] = []
-                    if isbn not in data["mw_info"][mw]["per_ig"][ig]:
-                        data["mw_info"][mw]["per_ig"][ig].append(isbn)
+                else:
+                    #print(det["d"]+" is malformed")
+                    if isbn is None:
+                        isbn = det["d"]
+            if isbn is None:
+                continue
+            if isbn not in data["isbn_info"]:
+                data["isbn_info"][isbn] = {}
+            data["isbn_info"][isbn] = {}
+            if mw not in data["isbn_info"][isbn]:
+                data["isbn_info"][isbn][mw] = {}
+            if w not in data["isbn_info"][isbn][mw]:
+                data["isbn_info"][isbn][mw][w] = {}
+            if ig not in data["isbn_info"][isbn][mw][w]:
+                data["isbn_info"][isbn][mw][w][ig] = []
+            data["isbn_info"][isbn][mw][w][ig].append(imgfname)
+            if mw not in data["mw_info"]:
+                data["mw_info"][mw] = {"from_db": [], "from_scans": [], "per_ig": {}, "ig_to_vnum": {}}
+            data["mw_info"][mw]["from_scans"].append(isbn)
+            if ig not in data["mw_info"][mw]["per_ig"]:
+                data["mw_info"][mw]["per_ig"][ig] = []
+            if isbn not in data["mw_info"][mw]["per_ig"][ig]:
+                data["mw_info"][mw]["per_ig"][ig].append(isbn)
 
 def handle_duplicates(data, stats):
     for isbn, isbn_data in data["isbn_info"].items():
@@ -133,72 +160,79 @@ def handle_differences(data, stats):
         stats["in_db_not_in_scans"] += len(from_db_not_in_scans)
         from_scans_not_in_db = set(mw_data["from_scans"]) - set(mw_data["from_db"])
         stats["in_scans_not_in_db"] += len(from_scans_not_in_db)
-        if from_db_not_in_scans and from_scans_not_in_db:
-            print(mw+" has isbns in db not in scans: "+", ".join(from_db_not_in_scans)+" and isbns in scans not in db: "+", ".join(from_scans_not_in_db))
-        elif from_db_not_in_scans:
-            print(mw+" has isbns in db not in scans: "+", ".join(from_db_not_in_scans))
-        elif from_scans_not_in_db:
-            print(mw+" has isbns in scans not in db: "+", ".join(from_scans_not_in_db))
+        #if from_db_not_in_scans and from_scans_not_in_db:
+        #    print(mw+" has isbns in db not in scans: "+", ".join(from_db_not_in_scans)+" and isbns in scans not in db: "+", ".join(from_scans_not_in_db))
+        #elif from_db_not_in_scans:
+        #    print(mw+" has isbns in db not in scans: "+", ".join(from_db_not_in_scans))
+        #elif from_scans_not_in_db:
+        #    print(mw+" has isbns in scans not in db: "+", ".join(from_scans_not_in_db))
         if len(mw_data["from_db"]) == 1 and len(mw_data["from_scans"]) == 1:
             if not well_formed(mw_data["from_db"][0]):
                 data["proposed_substitutions_malformed"].append([mw, mw_data["from_db"][0], mw_data["from_scans"][0]])
             elif mw_data["from_db"][0] != mw_data["from_scans"][0]:
-                data["proposed_substitutions"].append([mw, mw_data["from_db"][0], mw_data["from_scans"][0]])
+                if equivalent(mw_data["from_db"][0], mw_data["from_scans"][0]):
+                    data["new_isbns"].append([mw, mw_data["from_scans"][0], mw_data["from_db"][0], ""])
+                else:
+                    data["proposed_substitutions"].append([mw, mw_data["from_db"][0], mw_data["from_scans"][0]])
         if len(mw_data["from_db"]) == 1 and len(mw_data["from_scans"]) == 0 and not well_formed(mw_data["from_db"][0]):
             data["malformed_to_review"].append([mw, mw_data["from_db"][0]])
         if len(mw_data["from_db"]) == 0 and len(mw_data["from_scans"]) == 1:
-            data["new_isbns"].append([mw, mw_data["from_scans"][0]])
-    data["proposed_substitutions_malformed"] = sorted(data["proposed_substitutions_malformed"], key=lambda x: x[0])
-    data["proposed_substitutions"] = sorted(data["proposed_substitutions"], key=lambda x: x[0])
-    data["malformed_to_review"] = sorted(data["malformed_to_review"], key=lambda x: x[0])
-    with open('analysis/simple_substitutions_malformed.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-        for row in data["proposed_substitutions_malformed"]:
-            writer.writerow(row)
-    with open('analysis/simple_substitutions.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-        for row in data["proposed_substitutions"]:
-            writer.writerow(row)
-    with open('analysis/malformed_toreview.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-        for row in data["malformed_to_review"]:
-            writer.writerow(row)
-    with open('analysis/new_isbns.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-        for row in data["new_isbns"]:
-            writer.writerow(row)
+            data["new_isbns"].append([mw, mw_data["from_scans"][0], "", ""])
+
 
 def handle_multivolumes(data, stats):
     for mw, mwinfo in data["mw_info"].items():
         if len(mwinfo["ig_to_vnum"]) < 2 or len(mwinfo["per_ig"]) == 0:
             continue
         ordered_igs = sorted(mwinfo["ig_to_vnum"].keys(), key=lambda x: mwinfo["ig_to_vnum"][x])
+        all_isbns = []
+        for ig, isbn_list in mwinfo["per_ig"].items():
+            for isbn in isbn_list:
+                if isbn in all_isbns:
+                    continue
+                all_isbns.append(isbn)
         if len(mwinfo["ig_to_vnum"]) == len(mwinfo["per_ig"]):
             stats["found_all_volumes"] += 1
             stats["nb_volumes_found_after_first"] += len(mwinfo["per_ig"])
-            data["found_all_volumes"][mw] = []
-            for ig in ordered_igs:
-                data["found_all_volumes"][mw].append([mwinfo["ig_to_vnum"][ig], ", ".join(mwinfo["per_ig"][ig])])
+            if len(all_isbns) == 1:
+                if len(mwinfo["from_db"]) == 0:
+                    data["new_isbns"].append([mw, isbn_list[0], "", "found on all "+str(len(mwinfo["per_ig"]))+" volumes"])
+                elif len(mwinfo["from_db"]) == 1:
+                    if mwinfo["from_db"][0] != isbn_list[0]:
+                        if equivalent(mwinfo["from_db"][0], isbn_list[0]):
+                            data["new_isbns"].append([mw, isbn_list[0], mwinfo["from_db"][0], "found on all "+str(len(mwinfo["per_ig"]))+" volumes"])
+                        else:
+                            data["proposed_substitutions"].append([mw, mwinfo["from_db"][0], mwinfo["from_scans"][0], "found on all "+str(len(mwinfo["per_ig"]))+" volumes"])
+                else:
+                    data["proposed_substitutions"].append([mw, mwinfo["from_db"][0], mwinfo["from_scans"][0], "found on all "+str(len(mwinfo["per_ig"]))+" volumes"])
+            else:
+                if len(mwinfo["from_db"]) == 0 or (len(mwinfo["from_db"]) == 1 and has_equivalent_in(mwinfo["from_db"][0], all_isbns)):
+                    data["mutli_volumes_diff_isbn_no_review"][mw] = []
+                    for ig in ordered_igs:
+                        data["mutli_volumes_diff_isbn_no_review"][mw].append([mwinfo["ig_to_vnum"][ig], ", ".join(mwinfo["per_ig"][ig])])
+                else:
+                    data["mutli_volumes_diff_isbn_review"][mw] = []
+                    for ig in ordered_igs:
+                        data["mutli_volumes_diff_isbn_review"][mw].append([mwinfo["ig_to_vnum"][ig], ", ".join(mwinfo["from_db"]),", ".join(mwinfo["per_ig"][ig])])
         else:
             stats["found_not_all_volumes"] += 1
             stats["nb_volumes_found_after_first"] += len(mwinfo["per_ig"])
             stats["nb_volumes_not_found_after_first"] += len(mwinfo["ig_to_vnum"]) - len(mwinfo["per_ig"])
-            data["found_not_all_volumes"][mw] = []
-            for ig in ordered_igs:
-                if ig in mwinfo["per_ig"]:
-                    data["found_not_all_volumes"][mw].append([mwinfo["ig_to_vnum"][ig], ", ".join(mwinfo["per_ig"][ig])])
+            if len(all_isbns) == 1 and len(mwinfo["per_ig"]) > 1:
+                if len(mwinfo["from_db"]) == 0:
+                    data["new_isbns"].append([mw, isbn_list[0], "", "found on "+str(len(mwinfo["per_ig"]))+"/"+str(len(mwinfo["ig_to_vnum"]))+" volumes"])
+                elif len(mwinfo["from_db"]) == 1:
+                    if mwinfo["from_db"][0] != isbn_list[0]:
+                        if equivalent(mwinfo["from_db"][0], isbn_list[0]):
+                            data["new_isbns"].append([mw, isbn_list[0], mwinfo["from_db"][0], "found on "+str(len(mwinfo["per_ig"]))+"/"+str(len(mwinfo["ig_to_vnum"]))+" volumes (no other isbn found)"])
+                        else:
+                            data["proposed_substitutions"].append([mw, mwinfo["from_db"][0], mwinfo["from_scans"][0], "found on "+str(len(mwinfo["per_ig"]))+"/"+str(len(mwinfo["ig_to_vnum"]))+" volumes (no other isbn found)"])
                 else:
-                    data["found_not_all_volumes"][mw].append([mwinfo["ig_to_vnum"][ig], "?"])
-    with open('analysis/multi_all_volumes_found.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-        for mw in sorted(data["found_all_volumes"].keys()):
-            for e in data["found_all_volumes"][mw]:
-                writer.writerow([mw, e[0], e[1]])
-    with open('analysis/multi_not_all_volumes_found.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-        for mw in sorted(data["found_not_all_volumes"].keys()):
-            for e in data["found_not_all_volumes"][mw]:
-                writer.writerow([mw, e[0], e[1]])
+                    data["proposed_substitutions"].append([mw, mwinfo["from_db"][0], mwinfo["from_scans"][0], "found on all "+str(len(mwinfo["per_ig"]))+"/"+str(len(mwinfo["ig_to_vnum"]))+" volumes (no other isbn found)"])
+            else:
+                data["mutli_volumes_diff_isbn_review"][mw] = []
+                for ig in ordered_igs:
+                    data["mutli_volumes_diff_isbn_review"][mw].append([mwinfo["ig_to_vnum"][ig], ", ".join(mwinfo["from_db"]),", ".join(mwinfo["per_ig"][ig]) if ig in mwinfo["per_ig"] else "?"])
 
 def main():
     db = None
@@ -229,8 +263,8 @@ def main():
         "proposed_substitutions_malformed": [],
         "malformed_to_review": [],
         "new_isbns": [],
-        "found_all_volumes": {},
-        "found_not_all_volumes": {},
+        "mutli_volumes_diff_isbn_no_review": {},
+        "mutli_volumes_diff_isbn_review": {}
     }
     get_mw_infos(data)
     for w in tqdm(db):
@@ -240,9 +274,41 @@ def main():
             continue
         mw = w_to_mw[w]
         analyze_w(w, w_dbinfo, mw, data, stats)
-    handle_duplicates(data, stats)
+    # handle_duplicates(data, stats)
     handle_differences(data, stats)
     handle_multivolumes(data, stats)
+    data["proposed_substitutions_malformed"] = sorted(data["proposed_substitutions_malformed"], key=lambda x: x[0])
+    data["proposed_substitutions"] = sorted(data["proposed_substitutions"], key=lambda x: x[0])
+    data["malformed_to_review"] = sorted(data["malformed_to_review"], key=lambda x: x[0])
+    with open('analysis/simple_substitutions_malformed.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        for row in data["proposed_substitutions_malformed"]:
+            writer.writerow(row)
+    with open('analysis/simple_substitutions.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        for row in data["proposed_substitutions"]:
+            writer.writerow(row)
+    with open('analysis/malformed_toreview.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        for row in data["malformed_to_review"]:
+            writer.writerow(row)
+    with open('analysis/new_isbns.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        for row in data["new_isbns"]:
+            writer.writerow(row)
+    with open('analysis/mutli_volumes_diff_isbn_review.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        for mw in sorted(data["mutli_volumes_diff_isbn_review"].keys()):
+            for e in data["mutli_volumes_diff_isbn_review"][mw]:
+                writer.writerow([mw]+ e)
+            writer.writerow([])
+    with open('analysis/mutli_volumes_diff_isbn_no_review.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        for mw in sorted(data["mutli_volumes_diff_isbn_no_review"].keys()):
+            for e in data["mutli_volumes_diff_isbn_no_review"][mw]:
+                writer.writerow([mw]+ e)
+            writer.writerow([])
+
     stats["total"] = len(data["isbn_info"].keys())
     print(stats)
 
